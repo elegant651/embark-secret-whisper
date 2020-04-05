8278fc90f18d6b23111ea5fd9154344e2d76053d
elegant651 <korea651@gmail.com>
2020-04-05 17:06:44 +0900

    Connect to web3 with metamask

--- app/src/main.js
+++ app/src/main.js
@@ -10,6 +10,32 @@ Vue.use(VueChatScroll)
 
 Vue.config.productionTip = false
 
+Vue.mixin({
+  async created() {  	
+    console.log(await ethereum.send('net_version'))
+
+    try {
+      await ethereum.enable()
+
+      this.$web3 = new Web3(ethereum)      
+
+      this.$getDefaultAccount = () => {
+        return new Promise((resolve, reject) => {
+    	  this.$web3.eth.getAccounts((err, data) => {
+    	    if(!err) {
+    	      this.$web3.eth.defaultAccount = data[0]
+    	      resolve(data[0])
+    	    }
+    		reject(err)
+    	  })
+    	})
+      }
+    } catch (error) {
+      console.error('e', error)
+    }
+  }
+})
+
 new Vue({
   router,
   vuetify,
