3ebb98a09c27e114be71dc128559991ce6ca9b32
elegant651 <korea651@gmail.com>
2020-05-01 19:38:22 +0900

    change into httpprovider

--- app/src/services/WhisperService.js
+++ app/src/services/WhisperService.js
@@ -14,11 +14,13 @@ let keyPair, pubKey, channelSymKey = null
 let channelTopic = DEFAULT_TOPIC
 
 const PROVIDER_PRIVNET_URI = `ws://${process.env.VUE_APP_WHISPER_HOST}:8546`
+const PROVIDER_PRIVNET_HTTP_URI = `http://${process.env.VUE_APP_WHISPER_HOST}:8545`
 const PROVIDER_TESTNET_URI = `wss://ropsten.infura.io/ws/v3/${process.env.VUE_APP_INFURA_KEY}`
 
 export async function init() {
   try {
-    web3.setProvider(new Web3.providers.WebsocketProvider(PROVIDER_PRIVNET_URI, {headers: {Origin: "mychat"}}));
+    // web3.setProvider(new Web3.providers.WebsocketProvider(PROVIDER_PRIVNET_URI, {headers: {Origin: "mychat"}}));
+    web3.setProvider(new Web3.providers.HttpProvider(PROVIDER_PRIVNET_HTTP_URI));
     await web3.eth.net.isListening();
   } catch(e) {
     console.error(e)
