2e8c29acabc11407196d214efd450b44bfaec860
elegant651 <korea651@gmail.com>
2020-05-22 15:11:59 +0900

    Add PostAuction view for creating auction

---
+++ app/src/components/marketplace/PostAuction.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-form class="form" ref="form">      
+      <v-text-field
+      	v-model="tokenid"
+      	></v-text-field>
+
+        <v-text-field
+            v-model="auction.auctionTitle"
+            placeholder="e.g. My NFT"
+            label="Auction title"
+            persistent-hint
+            ></v-text-field>
+
+        <v-text-field
+            v-model="auction.price"
+            placeholder="e.g. 1"
+            label="Price"
+            type="number"
+            persistent-hint
+            ></v-text-field>
+
+        <v-btn @click="createAuction" outlined color="teal">Create Auction</v-btn>
+    </v-form>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  props: ['tokenid', 'metadata'],
+
+  data() {
+    return {
+      auction: {        
+        auctionTitle: '',
+        price: null        
+      },
+    }
+  },
+
+  computed: {
+    
+  },
+
+  methods: {
+    createAuction() {
+      if(!this.tokenid) {
+        alert("Check for tokenId")
+        return
+      }
+
+      
+    }    
+  }
+}
+</script>
