aa71a44979ea4afc84fc457567fdf81f6293b6b7
elegant651 <korea651@gmail.com>
2020-05-22 15:10:13 +0900

    Add marketplace view

---
+++ app/src/components/marketplace/Marketplace.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+  	<v-layout row wrap>
+      <v-flex v-for="(auction, index) in auctions" :key="index" xs4>
+      	<v-card>
+          <v-img :src="auction.image" height="200px"></v-img>
+          
+      		<div>Title: {{auction.title}}</div>
+          <div>Price: {{auction.price}} Ether</div>
+          <div>TokenId: {{auction.tokenId}}</div>
+          <div>Owner: {{auction.owner}}</div>
+          <div>Active: {{auction.active}}</div>
+          <div>Finalized: {{auction.finalized}}</div>        
+      	</v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+
+
+export default {
+  data() {
+    return {      
+      auctions: []
+    }
+  },
+
+  mounted() {
+    this.getAuctions()
+  },
+
+  methods: {
+    async getAuctions() {
+      
+    }	
+  }
+}
+</script>
