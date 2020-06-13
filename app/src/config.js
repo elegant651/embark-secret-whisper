57f1b6de5e56d266f62e6a861ee0e75d904290e3
elegant651 <korea651@gmail.com>
2020-06-13 15:19:42 +0900

    Set ABI for deployed contract

---
+++ app/src/config.js
@@ -0,0 +1,12 @@
+const MyNFT = require('../../dist/contracts/MyNFT.json')
+const Auctions = require('../../dist/contracts/Auctions.json')
+
+export default {
+	MYNFT_CA: MyNFT.deployedAddress,
+	AUCTIONS_CA: Auctions.deployedAddress,
+
+	MYNFT_ABI: MyNFT.abiDefinition,
+	AUCTIONS_ABI: Auctions.abiDefinition,
+
+	GAS_AMOUNT: 500000
+}
